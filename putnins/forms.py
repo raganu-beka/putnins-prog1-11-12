4d78e3a3a5500dbcc545036d48e2291bed0b989e
Maksims Piļščikovs <45118787+mpilscikov@users.noreply.github.com>
2023-11-10 10:31:18 +0200

    Create forms.py

---
+++ putnins/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import Length
+
+class UserRegisterForm(FlaskForm):
+  username = StringField('Username', validators=[Length(min=4, max=24)])
+  password = PasswordField('Password', validators=[Length(min=8, max=24)])
