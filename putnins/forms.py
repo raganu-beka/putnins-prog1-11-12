5e8d52ac1a765b8477a1c4718ce47c54afb6da27
raganu beka <m.pilscikov@gmail.com>
2023-11-17 08:28:33 +0200

    add form html

--- putnins/forms.py
+++ putnins/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import Length
+from wtforms.validators import Length, EqualTo
 
 class UserRegisterForm(FlaskForm):
-  username = StringField('Username', validators=[Length(min=4, max=24)])
-  password = PasswordField('Password', validators=[Length(min=8, max=24)])
+    username = StringField('Username',
+                         validators=[Length(min=4, max=24)])
+    password = PasswordField('Password',
+                           validators=[Length(min=8, max=24)])
+    confirm_password = PasswordField('Confirm password',
+                           validators=[Length(min=8, max=24),
+                                       EqualTo('password')])
