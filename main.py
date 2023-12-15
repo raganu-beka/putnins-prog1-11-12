deb65d05c9685e2618603469332a9f0969232310
raganu beka <m.pilscikov@gmail.com>
2023-12-15 09:03:41 +0200

    show likes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from putnins import app, Post, User, Comment
+from putnins import app, Post, User, Comment, Like
 
 
 if __name__ == '__main__':
     User.create_table(fail_silently=True)
     Post.create_table(fail_silently=True)
     Comment.create_table(fail_silently=True)
+    Like.create_table(fail_silently=True)
+    
     app.run(host='0.0.0.0', port=81, debug=True)
